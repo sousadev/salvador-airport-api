9b4fbac8880ef2fcd91c9d9e022d46d2a0227fa2
Italo Sousa <italo@italosousa.com.br>
2021-12-02 13:26:15 -0300

    Optimizing page index

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-import useSwr from 'swr'
-import Link from 'next/link'
+import useSwr from 'swr';
+import Link from 'next/link';
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
-  const { data, error } = useSwr('/api/users', fetcher)
+  const { data, error } = useSwr('/api/users', fetcher);
 
-  if (error) return <div>Failed to load users</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>Failed to load users</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
-    <ul>
-      {data.map((user) => (
-        <li key={user.id}>
-          <Link href="/user/[id]" as={`/user/${user.id}`}>
-            <a>{`User ${user.id}`}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  )
+    <div>
+      <h2>
+        API para visualizar os voos que chegam e que partem do Aeroporto
+        Deputado Luis Eduardo Magalhães em Salvador - BA
+      </h2>
+
+      <h3> Rotas de Chegada</h3>
+
+      <p>
+        /voos/chegadas/company/{'Nome da Companhia Aérea'} // Para visualizar{' '}
+      </p>
+    </div>
+  );
 }
